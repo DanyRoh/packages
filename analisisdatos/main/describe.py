ff149fe8f2ea81e5785777c350d2a61a46e4d492
Daniel Rodrigálvarez Morente <drodrigalvarez@uoc.edu>
2024-02-20 07:19:44 +0100

    Probando la actualización de código en github

--- analisisdatos/main/describe.py
+++ analisisdatos/main/describe.py
@@ -1,6 +1,11 @@
 
 def describe_df(df):
-    # print(f'El dataframe tiene {df.shape[0]} filas y {df.shape[1]} columnas.')
+    """
+    Facilita la informació necesario para conocer el contenido del dataframe
+    :param df: dataframe de la librería Pandas
+    :return: imprime diferentes descripciones del dataframe
+    """
+    print(f'El dataframe tiene {df.shape[0]} filas y {df.shape[1]} columnas.')
     print(df.describe())
     print(df.head())
     print(df.tail())
